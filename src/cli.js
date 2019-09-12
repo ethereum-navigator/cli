c57274080fc9a663a4001cd966d578e76b0c6b0b
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-09-12 13:59:31 +0200

    initial version

---
+++ src/cli.js
@@ -0,0 +1,22 @@
+const navigate = require('@ethereum-navigator/navigator').navigate;
+
+(async () => {
+
+    const json = true
+    const rpcUrl = process.argv[2]
+
+    const network = await navigate({
+        rpcUrl
+    })
+
+    if (json) {
+        console.log(JSON.stringify(network, null, 2))
+    } else {
+
+        if (network.name) {
+            console.log(`Connected to the '${network.name}' network of '${network.project}'.`)
+        } else {
+            console.log(`Network with id: ${network.networkId} is not in the atlas!`)
+        }
+    }
+})()
